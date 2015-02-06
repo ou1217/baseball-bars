93ea2187a3df345a50edcb9a3f3e0f20f368b98d
Sifan Ouyang <ouyangsifan@gmail.com>
2015-02-05 21:15:42 -0600

    This is updated for assignment three

--- js/script.js
+++ js/script.js
@@ -1,8 +1,32 @@
+var playerData;
 $(document).ready(function() {
-    console.log("Hello world.")
+   getData();
 });
 
+function getData(){
+    $.getJSON("js/juanbaseballcard.json",
+        function (data){
+            playerData = data;
+        drawChart();
+    });
 
+}
+
+
+function drawChart() {
+	$(".player-name").html(playerData.name);
+    $.each(playerData.stats, function(i, item) {
+		var width = item.HR * 10;
+		$(".chart").append(
+			"<div class='col-md-12 bar-container'>"+
+				"<div class='year'>"+item.year+"</div>"+
+				"<div class='bar' style='width: "+width+"px'></div>"+
+				"<div class='total'>"+item.HR+"</div>"+
+			"</div>"
+		);
+	});
+
+}
 
 
 
